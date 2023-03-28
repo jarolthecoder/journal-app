62dfc87991b09667f95822987a36150cacf42041
jarolthecoder <jarolr.webdev@gmail.com>
2023-03-28 14:14:38 -0400

    Updated

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 
@@ -10,6 +11,20 @@ export const JournalPage = () => {
 
       {/* <NothingSelectedView /> */}
       <NoteView />
+
+      <IconButton
+        size="large"
+        sx={{
+          color:'#fff',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          bottom: 50,
+          right: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
     </JournalLayout>
   )
 }
--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,12 +1,40 @@
-import { Grid, Typography } from "@mui/material"
-
+import { SaveOutlined } from "@mui/icons-material"
+import { Button, Grid, TextField, Typography } from "@mui/material"
+import { ImageGallery } from "../components"
 
 export const NoteView = () => {
   return (
-    <Grid container direction='row' sx={{ mb: 1 }}>
-      <Grid>
+    <Grid container direction='row' justifyContent="space-between" alignItems="center" sx={{ mb: 1 }}>
+      <Grid item>
         <Typography fontSize={ 39 } fontWeight="light">Aug 28, 2023</Typography>
       </Grid>
+      <Grid item>
+        <Button color="primary" sx={{ p: 2 }}>
+          <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+          Save
+        </Button>
+      </Grid>
+
+      <Grid container sx={{ mb: 2 }}>
+        <TextField
+          type="text" 
+          variant="filled"
+          fullWidth
+          label="Title"
+          placeholder="Add a title"
+          sx={{ border: 'none', mb: 1 }}
+        />
+        <TextField
+          type="text" 
+          variant="filled"
+          fullWidth
+          multiline
+          placeholder="What happened today?"
+          minRows={ 5 }
+        />
+      </Grid>
+
+      <ImageGallery />
     </Grid>
   )
 }
