04c3ea4687661fc54aa19b3d3f0a5f34fa0eb3bf
jarolthecoder <jarolr.webdev@gmail.com>
2023-04-04 09:35:15 -0400

    Login/out auth added

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,19 +3,29 @@ import { createSlice } from '@reduxjs/toolkit'
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    status: 'not-authenticated', // checking, not-authenticated, authenticated
+    status: 'authenticated', // checking, not-authenticated, authenticated
     uid: null,
     email: null,
     displayName: null,
     photoURL: null,
-    errorMessage:  null
+    errorMessage: null
   },
   reducers: {
-    login: (state, action) => {
-
+    login: (state, { payload }) => {
+      state.status = 'not-authenticated';
+      state.uid = payload.uid;
+      state.email = payload.email;
+      state.displayName = payload.displayName;
+      state.photoURL = payload.photoURL;
+      state.errorMessage = null;
     },
-    logout: (state, payload) => {
-
+    logout: (state, { payload }) => {
+      state.status = 'not-authenticated';
+      state.uid = null;
+      state.email = null;
+      state.displayName = null;
+      state.photoURL = null;
+      state.errorMessage = payload.errorMessage;
     },
     checkingCredentials: (state) => {
       state.status = 'checking';
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,5 +1,5 @@
 import { signInWithGoogle } from "../../firebase/providers"
-import { checkingCredentials } from "./authSlice"
+import { checkingCredentials, login, logout } from "./authSlice"
 
 export const checkingAuthentication = (email, password) => {
   return async(dispatch) => {
@@ -12,8 +12,10 @@ export const startGoogleSignIn = (email, password) => {
   return async(dispatch) => {
 
     dispatch(checkingCredentials());
-    
+
     const result = await signInWithGoogle();
-    console.log({result})
+    if (!result) return dispatch(logout(result.errorMessage));
+
+    dispatch(login())
   }
 }
