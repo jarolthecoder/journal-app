d9a8df71c4c9ab4a2dce1fb50d4d19b8c0b9e39b
jarolthecoder <jarolr.webdev@gmail.com>
2023-04-05 10:55:53 -0400

    useForm hook updated

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,9 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
-export const useForm = (initialForm = {}) => {
+export const useForm = (initialForm = {}, formValidations = {}) => {
 
-  const [formState, setFormState] = useState(initialForm)
+  const [formState, setFormState] = useState(initialForm);
+  const [formValidation, setFormValidation] = useState({})
+
+  useEffect(() => {
+    createValidators();
+  }, [formState]);
+  
 
   const onInputChange = ({ target }) => {
     const {name, value} = target
@@ -16,6 +22,14 @@ export const useForm = (initialForm = {}) => {
   const onResetForm = () => {
     setFormState(initialState)
   }
+
+  const createValidators = () => {
+    const formCheckedValues = {};
+
+    for (const formField of Object.keys(formValidations)) {
+      console.log(formField);
+    }
+  } 
 
   return {
     ...formState,
