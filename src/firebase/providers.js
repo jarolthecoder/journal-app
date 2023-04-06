d03e1138bf384503e3dbc1ca1ef582f3543b9c36
jarolthecoder <jarolr.webdev@gmail.com>
2023-04-06 14:51:35 -0400

    User authemtication firebase

--- src/firebase/providers.js
+++ src/firebase/providers.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup, updateProfile } from "firebase/auth";
 import { FirebaseAuth } from "./config";
 
 const googleProvider = new GoogleAuthProvider();
@@ -29,10 +29,16 @@ export const signInWithGoogle = async() => {
 
 export const registerUserWithEmailPassword = async({email, password, displayName}) => {
   try{
-    
+
     const resp = await createUserWithEmailAndPassword(FirebaseAuth, email, password);
     const {uid, photoURL} = resp.user;
-    console.log(resp);
+    
+    await updateProfile(FirebaseAuth.currentUser, {displayName});
+
+    return {
+      ok: true,
+      displayName, email, photoURL, uid
+    }
 
   } catch (error) {
     console.log(error);
