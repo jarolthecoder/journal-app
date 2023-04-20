7739255eb55dc8674af92550d14f101699885cae
jarolthecoder <jarolr.webdev@gmail.com>
2023-04-20 12:07:58 -0500

    check auth custom hook added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,26 +1,13 @@
-import React, { useEffect } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
-import { useDispatch, useSelector } from 'react-redux'
 import { CheckingAuth } from '../ui/components/CheckingAuth'
-import { onAuthStateChanged } from 'firebase/auth'
-import { FirebaseAuth } from '../firebase/config'
-import { login, logout } from '../store/auth/authSlice'
+import { useCheckAuth } from '../hooks'
 
 export const AppRouter = () => {
 
-  const {status} = useSelector(state => state.auth);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async(user) => {
-      if(!user) return dispatch(logout());
-      const {uid, email, displayName, photoURL} = user
-      dispatch(login({uid, email, displayName, photoURL}));
-    });
-  }, [])
   
+  const status = useCheckAuth();
 
   if(status === 'checking') {
     return <CheckingAuth />
@@ -33,7 +20,6 @@ export const AppRouter = () => {
         ? <Route path="/*" element={ <JournalRoutes /> } />
         : <Route path="/auth/*" element={ <AuthRoutes /> } />
       }
-
       <Route path='/*' element={<Navigate to='/auth/login' />} />
     </Routes>
   )
