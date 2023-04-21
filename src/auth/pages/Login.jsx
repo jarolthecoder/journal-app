818834034cb14c90ba529330bb0a3853800313bd
jarolthecoder <jarolr.webdev@gmail.com>
2023-04-21 06:34:00 -0500

    Animate css added to login/register

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -33,7 +33,7 @@ export const Login = () => {
 
   return (
     <AuthLayout title="Login">
-      <form onSubmit={ onFormSubmit }>
+      <form onSubmit={ onFormSubmit } className="animate__animated animate__fadeIn animate__faster">
         <Grid container>
           <Grid item xs={ 12 } sx={{ mt: 2 }}>
             <TextField
